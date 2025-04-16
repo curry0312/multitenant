4f460b9412f87e8150fef88ad647549670486c34
curry0312 <curry900312@gmail.com>
2025-04-16 23:28:09 +0800

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+
 
 export default function Home() {
   return (
-   <div>
+   <div className="p-4 space-y-4">
       <h1>Hello World</h1>
-      <Button variant="outline">Click me</Button>
+      <Button variant="elevated">Click me</Button>
+      <Progress value={33} />
+      <Input placeholder="Enter your name" />
     </div>
   );
 }
